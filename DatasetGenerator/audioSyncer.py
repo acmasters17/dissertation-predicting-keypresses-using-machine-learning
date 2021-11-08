00156f09dea34ee6e0d0400aaf4be2aa3d5c0034
Adam <mastersacm17@gmail.com>
2021-11-08 10:33:01 +0000

    Created an audio syncer file

---
+++ DatasetGenerator/audioSyncer.py
@@ -0,0 +1,3 @@
+# Need a function that can match audio from local recording to part in the teams meeting
+# If we then know when the local recording matches up / time stamp where they sync
+# Then we can crop the teams audio from there with the timestamps we have from the firebase stuff
