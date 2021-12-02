b7ee68bea9ec7b1c3c9952a902a7db174581fb85
Adam <mastersacm17@gmail.com>
2021-12-02 11:44:14 +0000

    Created class for score to refactor

---
+++ MachineLearningModels/Score.py
@@ -0,0 +1,6 @@
+class Score:
+    def __init__(self, accuracy, precision,f1,recall):
+        self.accuracy = accuracy
+        self.precision = precision
+        self.f1 = f1
+        self.recall = recall
