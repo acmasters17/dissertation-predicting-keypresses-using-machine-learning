6c7330840c408e364ca148b2845d7f93e97e68f7
Adam <mastersacm17@gmail.com>
2022-01-30 16:58:54 +0000

    c

---
+++ DatasetGenerator/audioSplitterV5.py
@@ -0,0 +1,73 @@
+import requests
+import sox
+from config import COMPUTER_ID, SECRET, SESSION_NAME, FILE_NAME
+from datetime import datetime
+
+print("Retrieving Key Press Data...")
+# Load key press timestamps
+ploads = {'secret': SECRET, 'id': COMPUTER_ID, 'sessionName': SESSION_NAME}
+r = requests.post(
+    'https://us-central1-dissertation---pkp.cloudfunctions.net/retrieveKeyPressDataForComputerNameSession', data=ploads)
+
+# Takes in value like 0:00:05.509253
+# and 0:01:05.761893 and converts it into just seconds
+def convertTimestampIntoFloat(timestamp: str):
+    minsAsSeconds = float(timestamp[2:4]) * 60
+    seconds = float(timestamp[5:])
+    return minsAsSeconds + seconds
+
+
+# Handle request
+if(r.ok):
+    print("Retrieved Key Press Data")
+    keyPressTimeStampArray = r.json()["keyPressData"]
+    print(len(keyPressTimeStampArray))
+
+    # Get Offset from local recording to teams recording
+    # In first demo that is 1 min 28 seconds.053 = 88.053
+    # Second is 2 min 43 seconds .682 = 163.682
+    offset = 163.682
+
+    # For every keypress timestamp split the meeting audio
+    for keypress in keyPressTimeStampArray:
+        if(keypress['keyPressed'] == "START SESSION"):
+            # Dont do anything as start of session key press
+            print("STARTING SPLITTING")
+        else:
+            print("Creating File for " + keypress['keyPressed'])
+            # Get start of key press time
+            keypressStartTime = offset + \
+                convertTimestampIntoFloat(keypress['timeStamp'])
+
+            # window we want is like 0.25 seconds either side of this time so we have a 0.5 second window
+            windowStartTime = keypressStartTime - 0.25
+            windowEndTime  = keypressStartTime + 0.5
+
+            
+
+
+            # create transformer
+            tfm = sox.Transformer()
+            # add a command to trim the audio and get our window
+            tfm.trim(windowStartTime, windowEndTime)
+            # Now we want to do onset detection / silence trimming so we just get the wave
+            # Remove Silence from start
+            tfm.silence(1)
+            # Remove Silence from end
+            tfm.silence(-1)
+            # create an output file.
+            time = datetime.now()
+            
+            if(keypress['keyPressed'] == "."):
+                tfm.build_file('./InputAudioFiles/' + FILE_NAME, './SplitAudioFiles/FullStop/' +
+                               'FullStop' + '_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav')
+            elif (keypress['keyPressed'] == "!" or keypress['keyPressed'] == "1"):
+                tfm.build_file('./InputAudioFiles/' + FILE_NAME, './SplitAudioFiles/1!/' +
+                               '1!' + '_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav')
+            else:
+                tfm.build_file('./InputAudioFiles/' + FILE_NAME, './SplitAudioFiles/' +
+                               keypress['keyPressed'] + '/' + keypress['keyPressed'] + '_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav')
+
+    print("FINISHED SPLITTING")
+else:
+    print("Failed getting keypress data")
