767f0a40268f45bed36436fed1e79001975507e7
Adam <mastersacm17@gmail.com>
2022-03-04 11:17:22 +0000

    V5 Pecussion Test

---
+++ DatasetGenerator/audioSplitterV5Pecussion.py
@@ -0,0 +1,96 @@
+import requests
+import sox
+from config import COMPUTER_ID, SECRET, SESSION_NAME, FILE_NAME
+from datetime import datetime
+import librosa
+import soundfile as sf
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+print("Retrieving Key Press Data...")
+# Load key press timestamps
+ploads = {'secret': SECRET, 'id': COMPUTER_ID, 'sessionName': SESSION_NAME}
+r = requests.post(
+    'https://us-central1-dissertation---pkp.cloudfunctions.net/retrieveKeyPressDataForComputerNameSession', data=ploads)
+
+# Takes in value like 0:00:05.509253
+# and 0:01:05.761893 and converts it into just seconds
+def convertTimestampIntoFloat(timestamp: str):
+    minsAsSeconds = float(timestamp[2:4]) * 60
+    seconds = float(timestamp[5:])
+    return minsAsSeconds + seconds
+
+def split(filepath):
+    sound = AudioSegment.from_wav(filepath)
+    dBFS = sound.dBFS
+    chunks = split_on_silence(sound, min_silence_len = 100,silence_thresh = dBFS)
+
+    print(len(chunks))
+    return chunks
+
+
+# Handle request
+if(r.ok):
+    print("Retrieved Key Press Data")
+    keyPressTimeStampArray = r.json()["keyPressData"]
+    print(len(keyPressTimeStampArray))
+
+    # Get Offset from local recording to teams recording
+    # In first demo that is 1 min 28 seconds.053 = 88.053
+    # Second is 2 min 40 seconds .890 = 160.890
+    offset = 160.890
+
+    # For every keypress timestamp split the meeting audio
+    for keypress in keyPressTimeStampArray:
+        if(keypress['keyPressed'] == "START SESSION"):
+            # Dont do anything as start of session key press
+            print("STARTING SPLITTING")
+        else:
+            print("Creating File for " + keypress['keyPressed'])
+            # Get start of key press time
+            keypressStartTime = offset + \
+                convertTimestampIntoFloat(keypress['timeStamp'])
+
+            # window we want is like 0.25 seconds either side of this time so we have a 0.5 second window
+            windowStartTime = keypressStartTime
+            windowEndTime  = keypressStartTime + 0.4
+
+            
+
+
+            # create transformer
+            tfm = sox.Transformer()
+            # add a command to trim the audio and get our window
+            tfm.trim(windowStartTime, windowEndTime)
+            tfm.gain(20)
+            # Build a temporary file
+            tfm.build_file('./InputAudioFiles/' + FILE_NAME, './window.wav')
+
+            # Run percussive audio splitter
+            y, sr = librosa.load('./InputAudioFiles/window.wav')
+            D = librosa.stft(y)
+            D_harmonic8, D_percussive8 = librosa.decompose.hpss(D, margin=8)
+            y_percussive8 = librosa.istft(D_percussive8, length=len(y))
+            sf.write("window8.wav",y_percussive8,22050)
+
+            # Run silence extractor
+            chunks  = split('./window8.wav')
+            # Save just the first silent chunk
+            silence_chunk = AudioSegment.silent(duration=500)
+            # Add the padding chunk to beginning and end of the entire chunk.
+            audio_chunk = silence_chunk + chunks[0] + silence_chunk
+        
+            # create an output file.
+            time = datetime.now()
+            if(keypress['keyPressed'] == "."):
+                audio_chunk.export('./SplitAudioFiles/FullStop/FullStop_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav', bitrate = "192k", format = "wav")
+            elif (keypress['keyPressed'] == "!" or keypress['keyPressed'] == "1"):
+                audio_chunk.export('./SplitAudioFiles/1!/1!_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav', bitrate = "192k", format = "wav")
+            elif (keypress['keyPressed'] == "\\"):
+                audio_chunk.export('./SplitAudioFiles/ForwardsSlash/ForwardsSlash_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav', bitrate = "192k", format = "wav")
+            else:
+                audio_chunk.export('./SplitAudioFiles/' + keypress['keyPressed'] + '/' + keypress['keyPressed'] + '_' + time.strftime("%d-%m-%Y_%H:%M%:%S:%f") + '.wav', bitrate = "192k", format = "wav")
+
+    print("FINISHED SPLITTING")
+else:
+    print("Failed getting keypress data")
--- Utilities/extractAudioFilesOffPecussion.py
+++ Utilities/extractAudioFilesOffPecussion.py
@@ -2,12 +2,14 @@ import wave
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 import numpy as np
-import soundfile as sf
+from scipy.fft import rfft, rfftfreq
 
 
 # Script takes in a specified audio file path and produces a mel-scaled spectrogram
-AUDIO_FILE_PATH = "../LiveProcessor/tempSplitFiles/0.wav"
+AUDIO_FILE_PATH = "./test8.wav"
 KEPRESS_LABEL = "Buffer File"
 
 # Load specified audio file
@@ -15,52 +17,44 @@ print("Loading audio file...")
 y, sr = librosa.load(AUDIO_FILE_PATH)
 print("Audio File Loaded")
 
-
-# Test
 fig, ax = plt.subplots(nrows=3, sharex=True)
 librosa.display.waveshow(y, sr=sr, ax=ax[0])
 ax[0].set(title='Mono View of Audio')
 ax[0].label_outer()
-# Create spectogram
-print("Creating Spectogram...")
-spectogram = librosa.feature.melspectrogram(y=y, sr=sr)
-print("Created Spectogram")
 
-y_harm, y_perc = librosa.effects.hpss(y)
-librosa.display.waveshow(y_harm, sr=sr, alpha=0.5, ax=ax[1], label='Harmonic')
-librosa.display.waveshow(y_perc, sr=sr, color='r', alpha=0.5, ax=ax[1], label='Percussive')
-ax[1].set(title='Multiple waveforms')
-ax[1].legend()
 
-# Passing through arguments to the Mel filters
-S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,
-                                    fmax=8000)
 
-S_dB = librosa.power_to_db(S, ref=np.max)
-img = librosa.display.specshow(S_dB, x_axis='time',
-                         y_axis='mel', sr=sr,
-                         fmax=8000, ax=ax[2])
-fig.colorbar(img, ax=ax[2], format='%+2.0f dB')
-ax[2].set(title="Mel-frequency spectrogram for " + KEPRESS_LABEL)
 plt.show()
 
 
-print("Saving Recording...")
-D = librosa.stft(y)
-D_harmonic2, D_percussive2 = librosa.decompose.hpss(D, margin=2)
-D_harmonic4, D_percussive4 = librosa.decompose.hpss(D, margin=4)
-D_harmonic8, D_percussive8 = librosa.decompose.hpss(D, margin=8)
-D_harmonic16, D_percussive16 = librosa.decompose.hpss(D, margin=16)
+def split(filepath):
+    sound = AudioSegment.from_wav(filepath)
+    dBFS = sound.dBFS
+    chunks = split_on_silence(sound, 
+        min_silence_len = 100,
+        silence_thresh = dBFS)
 
-y_percussive2 = librosa.istft(D_percussive2, length=len(y))
-y_percussive4 = librosa.istft(D_percussive4, length=len(y))
-y_percussive8 = librosa.istft(D_percussive8, length=len(y))
-y_percussive16 = librosa.istft(D_percussive16, length=len(y))
+    print(len(chunks))
+    return chunks
 
-sf.write("test2.wav",y_percussive2,22050)
-sf.write("test4.wav",y_percussive4,22050)
-sf.write("test8.wav",y_percussive8,22050)
-sf.write("test16.wav",y_percussive16,22050)
-print("Saved Recording")
+
+chunks  = split(AUDIO_FILE_PATH)
+
+# Process each chunk and export to a temp file
+for i, chunk in enumerate(chunks):
+    # Create a silence chunk that's 0.5 seconds (or 500 ms) long for padding.
+    silence_chunk = AudioSegment.silent(duration=500)
+
+    # Add the padding chunk to beginning and end of the entire chunk.
+    audio_chunk = silence_chunk + chunk + silence_chunk
+        
+    audio_chunk.export(
+        './testSplittingOffPercussion/' + str(i) + '.wav',
+        bitrate = "192k",
+        format = "wav"
+    )
+
+
+
 
 
