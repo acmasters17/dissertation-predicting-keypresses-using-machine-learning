6479f16c28d34194e511e9dd68b8d2ec886ab8de
Adam <mastersacm17@gmail.com>
2022-04-04 17:10:11 +0100

    Renaming Stuff For Clarity

--- MachineLearningModels/config.py
+++ MachineLearningModels/config.py
@@ -1,3 +1,3 @@
-INPUT_CSV_FILENAME = "alphanumerics_test_bed_2_splitting_off_timestamps.csv"
+INPUT_CSV_FILENAME = "alphanumerics_test_bed_2_combined_approach_first_wave_chosen.csv"
 RANDOM_STATE = 10
 FOLDS = 10
