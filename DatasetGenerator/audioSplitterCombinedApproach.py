607ca2849144c53b621dd79670559821256fa5f3
Adam <mastersacm17@gmail.com>
2022-04-04 13:00:11 +0100

    Switching back to first wave

--- DatasetGenerator/audioSplitterCombinedApproach.py
+++ DatasetGenerator/audioSplitterCombinedApproach.py
@@ -71,7 +71,7 @@ if(r.ok):
             # Save just the first silent chunk
             silence_chunk = AudioSegment.silent(duration=500)
 
-            if len(chunks) == 0 or len(chunks) > 1:
+            if len(chunks) == 0:
                 # skip
                 continue
             # Add the padding chunk to beginning and end of the entire chunk.
